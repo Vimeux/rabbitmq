d1fa66a7958a7f9f5b98c81d535986d33e29d333
Benjamin Vimeux <vimeux.benji@gmail.com>
2023-02-01 22:42:25 +0100

    clean

--- src/workers/index.js
+++ src/workers/index.js
@@ -1,10 +1,9 @@
 const start = (db, rabbitMQ) => {
   rabbitMQ.receiveStatus((status) => {
-    // get the id from the status after the dash
     const id = status.split('-')[1];
     console.log(id);
 
-    // // Mise à jour du status dans la BDD
+    // Mise à jour du status dans la BDD
     db('dishes').where({ id }).update({ status: 'Done' })
       .then(() => {
         console.log(`Le status de l'objet a été mis à jour avec succès en 'Done' pour l'ID ${id}`);
